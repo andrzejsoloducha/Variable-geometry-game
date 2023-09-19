dc843c3737205702e7a3124f2f276a7f16ada8a9
andrzejsoloducha <139710256+andrzejsoloducha@users.noreply.github.com>
2023-09-19 21:02:19 +0200

    player respawn at random locations
    to be fixed

---
+++ Assets/Scripts/PlayerGenerator.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+
+public class PlayerGenerator : MonoBehaviour
+{
+    public Tilemap tilemap;
+    public GameObject playerPrefab;
+    public Vector3Int cellPosition;
+    public int n;
+
+    private List<Vector3> availablePlaces;
+
+    void Start()
+    {
+        availablePlaces = new List<Vector3>();
+
+
+        for (int x = tilemap.cellBounds.xMin; x < tilemap.cellBounds.xMax; x++)
+        {
+            for (int y = tilemap.cellBounds.yMin; y < tilemap.cellBounds.yMax; y++)
+            {
+                Vector3Int cell = new Vector3Int(x, y, 0);
+                Vector3 position = tilemap.GetCellCenterWorld(cell);
+
+                if (tilemap.HasTile(cell))
+                {
+                    availablePlaces.Add(position);
+                }
+            }
+        }
+    }
+
+    void Respawn()
+    {
+        for (int i = 0; i < n; i++)
+        {
+            if (availablePlaces.Count > 0)
+            {
+                int index = Random.Range(0, availablePlaces.Count);
+
+                Vector3 position = availablePlaces[index];
+
+                Instantiate(playerPrefab, position, Quaternion.identity);
+
+                availablePlaces.RemoveAt(index);
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+}
