4b33fc335d7dcbaa6900d0e04e8d1f7661afcc23
andrzejsoloducha <139710256+andrzejsoloducha@users.noreply.github.com>
2023-09-20 02:09:55 +0200

    map generator updates
    map generates every launch of game using perlin noise
    deleted some files that wasn't needed

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+
+public class LevelGenerator : MonoBehaviour {
+
+	public Tilemap tilemap;
+	public TileBase tile;
+
+	public int width = 100;
+	public int height = 60;
+	
+	void Start()
+	{
+		ClearMap();
+		int[,] map = new int[width, height];
+		float seed = Time.time;
+
+		map = MapFunctions.GenerateArray(width, height, true);
+		map = MapFunctions.PerlinNoiseCave(map, Random.Range(0.0001f, 0.3f), true);
+				
+		
+		//Render the result
+		MapFunctions.RenderMap(map, tilemap, tile);
+	}
+
+	public void ClearMap()
+	{
+		tilemap.ClearAllTiles();
+	}
+}
---
+++ Assets/Scripts/MapFunctions.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class MapFunctions
+{
+    public static int[,] GenerateArray(int width, int height, bool empty)
+    {
+        int[,] map = new int[width, height];
+        for (int x = 0; x < map.GetUpperBound(0); x++)
+        {
+            for (int y = 0; y < map.GetUpperBound(1); y++)
+            {
+                if (empty)
+                {
+                    map[x, y] = 0;
+                }
+                else
+                {
+                    map[x, y] = 1;
+                }
+            }
+        }
+        return map;
+    }
+
+    public static void RenderMap(int[,] map, Tilemap tilemap, TileBase tile)
+    {
+        tilemap.ClearAllTiles();
+        for (int x = 0; x < map.GetUpperBound(0); x++)
+        {
+            for (int y = 0; y < map.GetUpperBound(1); y++)
+            {
+                if (map[x, y] == 1) // 1 = tile, 0 = no tile
+                {
+                    tilemap.SetTile(new Vector3Int(x, y, 0), tile);
+                }
+            }
+        }
+    }
+
+    public static int[,] PerlinNoise(int[,] map, float seed)
+    {
+        int newPoint;
+        float reduction = 0.5f;
+
+        for (int x = 0; x < map.GetUpperBound(0); x++)
+        {
+            newPoint = Mathf.FloorToInt((Mathf.PerlinNoise(x, seed) - reduction) * map.GetUpperBound(1));
+            newPoint += (map.GetUpperBound(1) / 2);
+
+            for (int y = newPoint; y >= 0; y--)
+            {
+                map[x, y] = 1;
+            }
+        }
+        return map;
+    }
+
+    public static int[,] PerlinNoiseSmooth(int[,] map, float seed, int interval)
+    {
+        if (interval > 1)
+        {
+            int newPoint, points;
+            float reduction = 0.5f;
+
+            Vector2Int currentPos, lastPos;
+
+            List<int> noiseX = new List<int>();
+            List<int> noiseY = new List<int>();
+
+            for (int x = 0; x < map.GetUpperBound(0); x += interval)
+            {
+                newPoint = Mathf.FloorToInt((Mathf.PerlinNoise(x, (seed * reduction))) * map.GetUpperBound(1));
+                noiseY.Add(newPoint);
+                noiseX.Add(x);
+            }
+
+            points = noiseY.Count;
+
+            for (int i = 1; i < points; i++)
+            {
+                currentPos = new Vector2Int(noiseX[i], noiseY[i]);
+                lastPos = new Vector2Int(noiseX[i - 1], noiseY[i - 1]);
+
+                Vector2 diff = currentPos - lastPos;
+                float heightChange = diff.y / interval;
+                float currHeight = lastPos.y;
+
+                for (int x = lastPos.x; x < currentPos.x; x++)
+                {
+                    for (int y = Mathf.FloorToInt(currHeight); y > 0; y--)
+                    {
+                        map[x, y] = 1;
+                    }
+                    currHeight += heightChange;
+                }
+            }
+        }
+        else
+        {
+            map = PerlinNoise(map, seed);
+        }
+
+        return map;
+    }
+
+
+    public static int[,] PerlinNoiseCave(int[,] map, float modifier, bool edgesAreWalls)
+    {
+        int newPoint;
+        for (int x = 0; x < map.GetUpperBound(0); x++)
+        {
+            for (int y = 0; y < map.GetUpperBound(1); y++)
+            {
+
+                if (edgesAreWalls && (x == 0 || y == 0 || x == map.GetUpperBound(0) - 1 || y == map.GetUpperBound(1) - 1))
+                {
+                    map[x, y] = 1;
+                }
+                else
+                {
+                    newPoint = Mathf.RoundToInt(Mathf.PerlinNoise(x * modifier, y * modifier));
+                    map[x, y] = newPoint;
+                }
+            }
+        }
+        return map;
+    }
+}
