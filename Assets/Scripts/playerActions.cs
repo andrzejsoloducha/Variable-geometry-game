e0b0aeb032aabb5e06364ba14798eff3e0c3a298
andrzejsoloducha <139710256+andrzejsoloducha@users.noreply.github.com>
2023-11-24 22:22:03 +0100

    fixed movement and jumps

---
+++ Assets/Scripts/playerActions.cs
@@ -0,0 +1,118 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class playerActions : MonoBehaviour
+{
+    private float horizontal;
+    private float speed = 8f;
+    private float jumpingPower = 16f;
+    private bool isFacingRight = true;
+
+    public int maxPlayers;
+    public GameObject[] allPlayers;
+
+    public bool actionTaken = false;
+
+    public GameObject turnScript;
+    public turnTimer scTimer;
+    private int currPlayer = 0;
+    public Rigidbody2D rb;
+    public bool grounded;
+
+    public Vector2 boxSize;
+    public float castDistance;
+    public LayerMask groundLayer;
+    
+
+    void Start()
+    {
+        allPlayers = GameObject.FindGameObjectsWithTag("Player");
+        maxPlayers = allPlayers.Length;
+
+        GameObject turnScript = GameObject.Find("turnTimer");
+        turnTimer scTimer = turnScript.GetComponent<turnTimer>();
+
+    }
+    
+    void Update()
+    {
+        if (scTimer) {
+            int currPlayer = scTimer.currentPlayer;
+        } else
+        {
+            Debug.Log("No game object called scTimer found");
+        }
+        
+        rb = allPlayers[currPlayer].GetComponent<Rigidbody2D>();
+
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+        }
+
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+
+        Flip();
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+    }
+
+    public bool isGrounded()
+    {
+        if (Physics2D.BoxCast(transform.position, boxSize, 0, -transform.up, castDistance, groundLayer))
+        {
+            return true;
+        }
+        else 
+        { 
+            return false; 
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position-transform.up * castDistance, boxSize)
+    }
+    //private void OnCollisionEnter2D(Collision2D other)
+    //{
+    //    if (other.gameObject.CompareTag("Ground"))
+    //    {
+    //        Vector3 normal = other.GetContact(0).normal;
+    //        if (normal == Vector3.up)
+    //        {
+    //            grounded = true;
+    //        }
+    //    }
+    //}
+
+    //private void OnCollisionExit2D(Collision2D other)
+    //{
+    //    if (other.gameObject.CompareTag("Ground"))
+    //    {
+    //        grounded = false;
+    //    }
+    //}
+
+    private void Flip()
+    {
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
+}
