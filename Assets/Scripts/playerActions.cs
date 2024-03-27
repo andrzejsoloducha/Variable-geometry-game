bd1f892ae7ebab61cd82ed1a301b0f6807d44b51
andrzejsoloducha <139710256+andrzejsoloducha@users.noreply.github.com>
2024-03-27 15:56:59 +0100

    unity smietnik nie da sie obrocic gracza z broniom

--- Assets/Scripts/playerActions.cs
+++ Assets/Scripts/playerActions.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -15,21 +17,34 @@ public class playerActions : MonoBehaviour
 
     public Rigidbody2D[] rigidbodies;
     public Vector2 boxSize = new Vector2(0.8f, 0.2f);
-    public float castDistance = 0.5f;
     public LayerMask groundLayer;
     public int currentPlayer;
     public GameObject[] players;
+    public GameObject player;
     public string playerLayerName = "Player";
+    public Vector3 scale;
     //private Vector3[] localScales;
 
     void Start()
     {
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
         GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
-
+        //Debug.Log("Number of players: " + players.Length);
         rigidbodies = new Rigidbody2D[players.Length];
         isFacingRight = new bool[players.Length];
 
+        // debugging players[]
+        //for (int i = 0; i < players.Length; i++)
+        //{
+        //    if (playersList[i] != null && playersList[i].activeSelf)
+        //    {
+        //        Debug.Log("znalaz³em aktywnego gracza: " + playersList[i].name);
+        //    }
+        //    else
+        //    {
+        //        Debug.LogWarning("znalazlem nulla jebanego");
+        //    }
+        //}
         for (int i = 0; i < players.Length; i++)
         {
             rigidbodies[i] = players[i].GetComponent<Rigidbody2D>();
@@ -41,9 +56,11 @@ public class playerActions : MonoBehaviour
     void Update()
     {
         int currentPlayer = gameManager.currentPlayer;
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+        GameObject player = players[currentPlayer]; // DLACZEGO TO KURWA NULLUJE
 
         Rigidbody2D rb = rigidbodies[currentPlayer];
-
+        Debug.Log("Current player index: " + currentPlayer);
         horizontal = Input.GetAxisRaw("Horizontal");
 
         if (Input.GetButtonDown("Jump") && isGrounded())
@@ -56,7 +73,25 @@ public class playerActions : MonoBehaviour
             rigidbodies[currentPlayer].velocity = new Vector2(rigidbodies[currentPlayer].velocity.x, rigidbodies[currentPlayer].velocity.y * 0.5f);
         }
 
-        Flip(currentPlayer, players);
+        if (player != null)
+        {
+            Flip(currentPlayer, player);
+        }
+        else
+        {
+            Debug.LogError("player jest nullem");
+        }
+        //if (players.Length > 0 && playersList[currentPlayer] != null)
+        //{
+        //    GameObject player = playersList[currentPlayer];
+        //    Debug.Log("Player name: " + player.name);
+        //    Flip(currentPlayer, player);
+        //}
+        //else
+        //{
+        //    Debug.LogError("players[] jest jebanym nullem???");
+        //}
+
 
     }
 
@@ -81,7 +116,7 @@ public class playerActions : MonoBehaviour
 
         if (!grounded)
         {
-            RaycastHit2D hit = Physics2D.Raycast(position + offset, Vector2.down, size.y / 2f + 0.1f, playerLayer); // need to be fixed later
+            RaycastHit2D hit = Physics2D.Raycast(position + offset, Vector2.down, size.y / 2f + 0.1f, playerLayer); // tu jest coœ zjebane, do poprawki potem
             if (hit.collider != null)
             {
                 Debug.Log("Hit object: " + hit.collider.gameObject.name);
@@ -93,14 +128,18 @@ public class playerActions : MonoBehaviour
     }
 
 
-    private void Flip(int currentPlayer, GameObject[] players)
+    private void Flip(int currentPlayer, GameObject player)
     {
         if ((isFacingRight[currentPlayer] && horizontal < 0f) || (!isFacingRight[currentPlayer] && horizontal > 0f))
         {
+            //Vector3 currentRotation = player.transform.localEulerAngles;
+            //currentRotation.y += 180f;
+            //player.transform.localEulerAngles = currentRotation;
+
             isFacingRight[currentPlayer] = !isFacingRight[currentPlayer];
-            Vector3 localScale = ;
-            localScale.x *= -1f;
-            players[currentPlayer].transform.localScale = localScale;
+            Vector3 scale = player.transform.localScale;
+            scale.x *= -1f;
+            player.transform.localScale = scale;
         }
     }
 }
